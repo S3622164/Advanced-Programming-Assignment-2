13e01136a0a2a78b8f8e3da84b150c674b5bd2e1
rmit-s3622164-seby-sebastian <sebytom@192.168.1.10>
2017-05-19 22:15:13 +1000

    fourth commit

---
+++ Assignment 2-1/src/Ozlympics/TestCases.java
@@ -0,0 +1,28 @@
+package Ozlympics;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.Test;
+
+public class TestCases {
+
+	
+	
+
+	public class CyclingTest {
+
+		/**
+		 * Test to check addPoints(int newPoints)
+		 * 
+		 */
+		@Test
+		public void testgetMinTime() {
+			Cycling cycle = new Cycling();
+			cycle.getMinTime();
+			assertTrue(cycle.getMinTime()==5);
+		}
+		
+	
+
+	}}
